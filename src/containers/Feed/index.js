0770630d5c44f3352593a876db14c76ae5c3d610
Brandon <brandilla1337@gmail.com>
2019-12-23 00:10:17 -0500

    Added IE exception

--- src/containers/Feed/index.js
+++ src/containers/Feed/index.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { sampleButtonClick } from "../../Redux/modules/sample";
+import { sampleButtonClick } from "../../redux/modules/sample";
 
 import Thumbnail from "../../components/Thumbnail";
 import "./feed.scss";
-
-// 480x272 thumbnail size
 
 class Feed extends Component {
     componentDidMount() {}
 
     render() {
         return (
-            <section>
+            <section className="Feed">
                 <Thumbnail />
-                <button onClick={this.props.sampleButtonClick}>Test redux action</button>
-
-                <pre>{JSON.stringify(this.props.sampleModule)}</pre>
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                <Thumbnail />
+                {/* <button onClick={this.props.sampleButtonClick}>Test redux action</button> */}
             </section>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 
-import configureStore, { history } from "./Redux/store";
+import configureStore, { history } from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 import "./global.scss";
 
@@ -14,17 +14,31 @@ import Feed from "./containers/Feed";
 //Components
 import Navbar from "./components/Navbar";
 
-const Index = () => (
-    <Provider store={configureStore()}>
-        <ConnectedRouter history={history}>
-            <Navbar />
-            <Switch>
-                <Redirect from="/" exact to="/feed" />
-                <Route exact path="/feed" component={Feed} />
-            </Switch>
-        </ConnectedRouter>
-    </Provider>
-);
+const isIE = () => {
+    var ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+    var msie = ua.indexOf("MSIE "); // IE 10 or older
+    var trident = ua.indexOf("Trident/"); //IE 11
+
+    return msie > 0 || trident > 0;
+};
+
+let Index;
+if (isIE()) {
+    //TODO: Create an browser upgrade landing page.
+    Index = () => <div>You need to upgrade your shit bruh.</div>;
+} else {
+    Index = () => (
+        <Provider store={configureStore()}>
+            <ConnectedRouter history={history}>
+                <Navbar />
+                <Switch>
+                    <Redirect from="/" exact to="/feed" />
+                    <Route exact path="/feed" component={Feed} />
+                </Switch>
+            </ConnectedRouter>
+        </Provider>
+    );
+}
 
 ReactDOM.render(<Index />, document.getElementById("root"));
 
