228a1d1e09859f45481e1de98f9dc8965cb8f963
Brandon <brandilla1337@gmail.com>
2020-03-08 12:21:36 -0400

    Added proptype

--- src/Components/SectionHeader/index.js
+++ src/Components/SectionHeader/index.js
@@ -1,11 +1,35 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { updatePeriod } from '../../Redux/topClips';
 import './sectionheader.scss';
 
-import { usePeriodSelection } from '../../Hooks/usePeriodSelection';
-
 export const SectionHeader = (props) => {
+	const periodEnum = [
+		{ id: 0, text: 'today', key: 'day' },
+		{ id: 1, text: 'this week', key: 'week' },
+		{ id: 2, text: 'this month', key: 'month' },
+		{ id: 3, text: 'of all time', key: 'all' }
+	];
+
 	const [showMenu, setShowMenu] = useState(false);
-	const { periodText, menu } = usePeriodSelection(props.from, setShowMenu);
+	const dispatch = useDispatch();
+	const periods = useSelector((state) => state.topClips[props.from]);
+
+	const key = periods.currentPeriod;
+	const current = periodEnum.filter((obj) => obj.key === key);
+	const periodText = current[0].text;
+
+	const updateMenu = (item) => {
+		dispatch(updatePeriod({ from: props.from, newPeriod: item.key }));
+		setShowMenu(false);
+	};
+
+	const menu = periodEnum.map((item) => (
+		<span key={item.id} onClick={() => updateMenu(item)} className='sort-item'>
+			{item.key}
+		</span>
+	));
 
 	return (
 		<div className='SectionHeader'>
@@ -16,6 +40,11 @@ export const SectionHeader = (props) => {
 			{showMenu && menu}
 		</div>
 	);
+};
+
+SectionHeader.propTypes = {
+	from: PropTypes.string.isRequired,
+	titleText: PropTypes.string.isRequired
 };
 
 export default SectionHeader;
